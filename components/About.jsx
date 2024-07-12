6450a5e2b367de61ee04b0a638ea256164380181
Rohit Rakesh <130208248+journeytoday@users.noreply.github.com>
2024-07-12 08:35:35 +0200

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -100,7 +100,7 @@ const About = () => {
           <div className='hidden xl:flex flex-1 relative'>
             <DevImg
               containerStyles='bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative'
-              imgSrc='/about/developer.png'
+              imgSrc='/about/developers.png'
             />
           </div>
           {/* tabs */}
